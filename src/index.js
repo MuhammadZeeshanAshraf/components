1ab6ccdeab21082f420157de9ff0dd69c0d42ad6
Muhammad Zeeshan Ashraf <zeeshanashraf633b@gmail.com>
2021-10-11 14:17:31 +0500

    Component Nesting , Reuseability and Configuration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
+
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 export default function App() {
-  return <div>Hi There</div>;
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are are sure man?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 6:00PM"
+          text="abc"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="AA"
+          timeAgo="Today at 7:00PM"
+          text="abdj"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="AM"
+          timeAgo="Today at 8:00PM"
+          text="shfh"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
